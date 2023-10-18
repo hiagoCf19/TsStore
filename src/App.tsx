975eac3bd2c98691b45573d504d4063f66b3c1f2
Hiago <hiagoferreira@id.uff.br>
2023-10-18 14:11:28 -0300

    feat: categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyles } from "./Styles/LightTheme";
 import { ResetCSS } from "./Styles/Reset";
 import { Banner } from "./components/Banner/Banner";
+import { Category } from "./components/Category/Categorias";
 import { Header } from "./components/Header/Header";
 import "./global.css";
 function App() {
@@ -8,8 +9,11 @@ function App() {
     <>
       <ResetCSS />
       <GlobalStyles />
-      <Header />
-      <Banner />
+      <div className="flex flex-col gap-6">
+        <Header />
+        <Banner />
+        <Category />
+      </div>
     </>
   );
 }
---
+++ src/components/Category/Categorias.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface Roupas {
+  nome: string;
+  image: string;
+}
+const roupas: Roupas[] = [
+  {
+    nome: "Camisetas",
+    image: "Assets/Mobile/Categorias/CamisetaMb.png",
+  },
+  {
+    nome: "Bolsas",
+    image: "Assets/Mobile/Categorias/Categoria Bolsas.png",
+  },
+  {
+    nome: "Calçados",
+    image: "Assets/Mobile/Categorias/Categoria calçados.png",
+  },
+  {
+    nome: "Calças",
+    image: "Assets/Mobile/Categorias/Categoria calças.png",
+  },
+  {
+    nome: "Casacos",
+    image: "Assets/Mobile/Categorias/Categoria Casacos.png",
+  },
+  {
+    nome: "Óculos",
+    image: "Assets/Mobile/Categorias/Categoria óculos.png",
+  },
+];
+export const Category: React.FC = () => {
+  return (
+    <>
+      <section className="flex mx-8">
+        <div className="flex flex-col gap-5 w-full">
+          <h1 className="font-medium text-[1.225rem] flex justify-center ">
+            Busque por categoria:
+          </h1>
+          <div className="flex flex-wrap gap-6">
+            {roupas.map((item: Roupas, i: number) => (
+              <div className="w-[146px] h-[192px] flex flex-col" key={i}>
+                <img src={item.image} alt={item.nome} />
+                <div className="bg-primary h-full text-[#FFF] flex justify-center items-center text-lg font-medium">
+                  {item.nome}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
