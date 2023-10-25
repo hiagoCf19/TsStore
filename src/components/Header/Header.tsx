98811175616d2475f25314d7d60dcf5cdc88e8b1
Hiago <hiagoferreira@id.uff.br>
2023-10-25 00:36:24 -0300

    fix:sheetCpn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,3 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { MenuIcon } from "lucide-react";
 import { SearchStyled } from "../UiVerse/SearchStyled";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -21,32 +12,7 @@ export const Header = () => {
             alt="logo"
             className="sm:w-[140px] w-[80px] mt-3"
           />
-          <div className="sm:hidden  mx-4">
-            <Sheet>
-              <SheetTrigger>
-                <MenuIcon color="#875cff" className="flex mt-3" />
-              </SheetTrigger>
-              <SheetContent className="bg-background">
-                <SheetHeader className="gap-8">
-                  <SheetTitle className="text-white text-[1.3rem] py-8">
-                    Menu
-                  </SheetTitle>
-
-                  <SheetDescription className="flex flex-col gap-14 font-medium text-[1rem]  ">
-                    {navRoutes.map((topico, i: number) => (
-                      <Link
-                        className="hover:text-roxo outline-none"
-                        key={i}
-                        to={topico.link}
-                      >
-                        {topico.title}
-                      </Link>
-                    ))}
-                  </SheetDescription>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
-          </div>
+          <div className="sm:hidden  mx-4"></div>
         </div>
         <nav className="text-black gap-[6.25rem] flex text-[1.195rem] font-semibold">
           {navRoutes.map((topico, i: number) => (
