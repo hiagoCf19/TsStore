0603605fa4edd27f28eaab2308efe74bcff6e870
hiagoCf19 <hiagoferreira@id.uff.br>
2023-11-01 21:22:24 -0300

    fix: card login

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -2,16 +2,51 @@ import { Galaxy } from "@/Styles/Galaxy";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background: rgba(0, 0, 0, 0.45);
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(16px);
-  -webkit-backdrop-filter: blur(16px);
-  border-radius: 6px;
   width: 90%;
   height: 90%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  transition: all 0.2s;
+  position: relative;
+  cursor: pointer;
+
+  .card-inner {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.161);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.85);
+    backdrop-filter: blur(5px);
+    border-radius: 8px;
+  }
+  .circle {
+    width: 200px;
+    height: 200px;
+    background: radial-gradient(#725cff, #110949);
+    border-radius: 50%;
+    position: absolute;
+    animation: move-up6 2s ease-in infinite alternate-reverse;
+  }
+
+  .circle:nth-child(1) {
+    top: 10px;
+    left: 10px;
+  }
+
+  .circle:nth-child(2) {
+    bottom: 10px;
+    right: 10px;
+    animation-name: move-down1;
+  }
+
+  @keyframes move-up6 {
+    to {
+      transform: translateY(-10px);
+    }
+  }
+
+  @keyframes move-down1 {
+    to {
+      transform: translateY(10px);
+    }
+  }
 `;
 
 export const LoginCpn = () => {
@@ -20,14 +55,10 @@ export const LoginCpn = () => {
       <Galaxy />
       <section className="h-[100vh]">
         <div className="flex justify-center items-center h-full">
-          <Container>
-            {/* DESKTOP */}
-            <div className="p-8 flex gap-6 w-full sm:h-full">
-              <div className="flex-1 border border-solid hidden sm:block">
-                1
-              </div>
-              <div className="flex-1 border border-solid">2</div>
-            </div>
+          <Container className="card">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="card-inner">aaa</div>
           </Container>
         </div>
       </section>
