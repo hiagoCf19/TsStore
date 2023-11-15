615620d7dbf4c9ffd1146607ad0b43c0f0581878
hiagoCf19 <hiagoferreira@id.uff.br>
2023-11-14 21:40:40 -0300

    fix: emailDesconto

--- src/components/e-mail/EmailDesconto.tsx
+++ src/components/e-mail/EmailDesconto.tsx
@@ -8,7 +8,9 @@ export const Cadastre = () => {
   const { userLogado } = useContext(ControlLog);
   console.log(userLogado);
   return (
-    <section className={` mx-5 sm:mx-[6.25rem] sm:flex sm:justify-center `}>
+    <section
+      className={` mx-5 sm:mx-[6.25rem] sm:flex sm:justify-center bg-transp2 `}
+    >
       <div className="flex flex-col  gap-6 border border-solid border-roxo p-5 sm:w-[45rem] sm:items-center rounded-lg ">
         <p className="text-center font-normal text-base text-[#a7a7a7] sm:text-lg">
           Quer receber nossas novidades, promoções exclusivas e 15% OFF na
